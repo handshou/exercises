1690a6f0d93793c469b0c7134d36eb715ee43c91
hansel <handshou@users.noreply.github.com>
2024-11-15 09:52:10 +0800

    feat(quadtree): use testMatch algorithm

--- quadtree/main.js
+++ quadtree/main.js
@@ -37,13 +37,34 @@ const ex_output_2 = [
   [1, 0], [1, 0], [1, 1], [1, 1]
 ]
 
-console.log('ex_1')
+ /**
+  * @param {number[][]} output - Actual response.
+  * @param {number[][]} expected - Correct response.
+  * @return {boolean} True when actual matches correct response.
+  */
+const testMatch = function(output, expected) {
+  if(output.length !== expected.length)
+    return false
+
+  const matchValues = output.map((out, index) => {
+      if(out[0] === 0 && expected[index][0] === 0)
+        return true
+
+      if(out[0] === 1 && expected[index][0] === 1 && out[1] === expected[index][1])
+        return true
+    return false
+  })
+
+  return matchValues.every(value => value)
+}
+
 let qtree_1 = new QuadTree(ex_grid_1)
+console.log('test_1', testMatch(qtree_1.render(), ex_output_1))
 console.log('my_output:', qtree_1.render())
 console.log('ex_output:', ex_output_1)
 
-console.log('ex_2')
 let qtree_2 = new QuadTree(ex_grid_2)
+console.log('test_2', testMatch(qtree_2.render(), ex_output_2))
 console.log('my_output_2:', qtree_2.render())
 console.log('ex_output:', ex_output_2)
 
@@ -103,8 +124,6 @@ const Node = function(grid, x, y, len) {
 
   this.processLeaf()
 }
-
-// console.log('recursion')
 
 const recursion_1 = new Node(ex_grid_1, 0, 0, ex_grid_1.length)
 // console.log('my_output_1 -', recursion_1.render())
