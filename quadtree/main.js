bdd5592f4d869272bfc6b3c6561deebd04401296
hansel <handshou@users.noreply.github.com>
2024-11-17 01:42:21 +0800

    feat(quadtree): solve

--- quadtree/main.js
+++ quadtree/main.js
@@ -6,20 +6,20 @@ let testMatch
 
 // Main function
 const main = () => {
-  let qtree_1 = new QuadTree2(ex_grid_1)
-  let result_1 = testMatch(qtree_1.render(), ex_output_1)
+  let qtree_1 = new QuadTree2(ex_grid_1).render()
+  let result_1 = testMatch(qtree_1, ex_output_1)
   console.log('test_1', result_1)
   if (!result_1) {
-    console.log('my_output:', qtree_1.render())
-    console.log('ex_output:', ex_output_1)
+    console.log('my_output:', qtree_1.map(item => (item === null ? "null" : item)))
+    console.log('ex_output:', ex_output_1.map(item => (item === null ? "null" : item)))
   }
 
-  let qtree_2 = new QuadTree2(ex_grid_2)
-  let result_2 = testMatch(qtree_2.render(), ex_output_2)
+  let qtree_2 = new QuadTree2(ex_grid_2).render()
+  let result_2 = testMatch(qtree_2, ex_output_2)
   console.log('test_2', result_2)
   if (!result_2) {
-    console.log('my_output:', qtree_2.render())
-    console.log('ex_output:', ex_output_1)
+    console.log('my_output:', qtree_2.map(item => (item === null ? "null" : item)))
+    console.log('ex_output:', ex_output_2.map(item => (item === null ? "null" : item)))
   }
 }
 
@@ -52,17 +52,23 @@ ex_output_2 = [
   [1, 0], [1, 0], [1, 1], [1, 1]
 ]
 
+ex_grid_3 = [
+  [0, 1, 0, 0],
+  [1, 0, 0, 0],
+  [1, 0, 0, 0],
+  [1, 0, 0, 0],
+]
+
 // Populate testMatch function
 testMatch = function(output, expected) {
   if (output.length !== expected.length) return false
-
   const matchValues = output.map((out, index) => {
-    if (out[0] === 0 && expected[index][0] === 0) return true
-    if (out[0] === 1 && expected[index][0] === 1 && out[1] === expected[index][1])
+    if (out === null && expected[index] === null) return true
+    if (out && out[0] === 0 && expected[index][0] === 0) return true
+    if (out && out[0] === 1 && expected[index][0] === 1 && out[1] === expected[index][1])
       return true
     return false
   })
-
   return matchValues.every(value => value)
 }
 
--- quadtree/quadtree_concise.js
+++ quadtree/quadtree_concise.js
@@ -1,54 +1,157 @@
 class QuadTree {
-  constructor(grid, x = 0, y = 0, len = grid.length) {
+  constructor(grid, x = 0, y = 0, len = grid.length, degree = 0) {
     this.grid = grid
+    this.degree = degree
     this.x = x
     this.y = y
     this.len = len
     this.isRoot = (this.grid.length === this.len)
+    this.isProcessed = 0
 
     this.val = 0
-    this.isLeaf = 1
+    this.isLeaf = 0
     this.tl = null
     this.tr = null
     this.bl = null
     this.br = null
-
-    this.processLeaf()
   }
 
-  processLeaf() {
-    this.val = this.grid[this.y][this.x]
-    for (let i = this.y; i < this.y + this.len; i++) {
-      for (let j = this.x; j < this.x + this.len; j++) {
-        const element = this.grid[i][j]
-        if (element !== this.val) {
-          this.isLeaf = 0
-          return
-        }
+  processNode() {
+    const hasChildren = this.tl && this.tr && this.bl && this.br
+    if (hasChildren) {
+      const childrenValuesSame = this.tl.getVal() === this.tr.getVal() &&
+        this.tr.getVal() === this.bl.getVal() &&
+        this.bl.getVal() === this.br.getVal()
+      const childrenAreLeaves = this.tl.getIsLeaf() === this.tr.getIsLeaf() &&
+        this.tr.getIsLeaf() === this.bl.getIsLeaf() &&
+        this.bl.getIsLeaf() === this.br.getIsLeaf()
+      if (childrenAreLeaves && childrenValuesSame) {
+        this.isLeaf = 1
+        this.val = this.tl.getVal()
       }
+      if (!childrenAreLeaves) {
+        this.isLeaf = 0
+      }
+      this.isProcessed = 1
+      return
     }
+    this.val = this.grid[this.y][this.x]
+    this.isLeaf = 1
+    this.isProcessed = 1
   }
 
   split() {
     const halfLen = this.len / 2
-    this.tl = new QuadTree(this.grid, this.x, this.y, halfLen)
-    this.tr = new QuadTree(this.grid, this.x + halfLen, this.y, halfLen)
-    this.bl = new QuadTree(this.grid, this.x, this.y + halfLen, halfLen)
-    this.br = new QuadTree(this.grid, this.x + halfLen, this.y + halfLen, halfLen)
+    this.tl = new QuadTree(this.grid, this.x, this.y, halfLen, this.degree + 1)
+    this.tr = new QuadTree(this.grid, this.x + halfLen, this.y, halfLen, this.degree + 1)
+    this.bl = new QuadTree(this.grid, this.x, this.y + halfLen, halfLen, this.degree + 1)
+    this.br = new QuadTree(this.grid, this.x + halfLen, this.y + halfLen, halfLen, this.degree + 1)
   }
 
   print() {
     return [this.isLeaf, this.val]
   }
 
+  getVal() {
+    return this.val
+  }
+
+  getIsLeaf() {
+    return this.isLeaf
+  }
+
+  getChildren() {
+    return [this.tl, this.tr, this.bl, this.br]
+  }
+  logBase(n, base = Math.E) { Math.log(n) / Math.log(base) }
+
+  build(result = [], stack = new Array(this.logBase(this.len * this.len, 4))) {
+    // breadth first search - gud
+    // number of levels = log~4 n = log~4(len * len)
+    // use stack, if no children, split recursively and add to stack
+    // if grid size === 1, process and pop next on stack
+    // if have children, process and pop next on stack
+    // if root, print and go to top left child 
+
+    // depth first search - no gud
+    if (!this.isProcessed) {
+      if (this.len === 1) {
+        this.processNode()
+        return
+      }
+
+      // quadtree construction does not have to follow BFS, can be DFS
+      const hasChildren = this.tl && this.tr && this.bl && this.br
+      if (!hasChildren) {
+        if (!stack[this.degree]) stack[this.degree] = []
+        stack[this.degree].push(this)
+        this.split()
+        this.tl.build([], stack)
+        this.tr.build([], stack)
+        this.bl.build([], stack)
+        this.br.build([], stack)
+      }
+
+      // quadtree processing 
+      stack.forEach(level => {
+        level.forEach(node => {
+          node.processNode()
+        })
+      })
+
+    }
+
+    //stack.forEach((level, stack_index) => {
+    //  level.forEach((node) => {
+
+    //    result.push(node.print())
+    //    stack[stack_index].pop()
+    //    node.getChildren().forEach(child => {
+
+    //      result.push(child.print())
+    //    })
+    //  })
+    //})
+
+    //if (!this.isLeaf) {
+    //  result.push(this.tl.print())
+    //  result.push(this.tr.print())
+    //  result.push(this.bl.print())
+    //  result.push(this.br.print())
+    //}
+
+    // result processing
+    // printing has to be breadth first, the tail null values are discarded
+    // discarded by popping all tail leaf nodes
+    // when node with isLeaf is false, print children nodes tl tr bl br 
+    // if isLeaf is true, print null for children nodes
+
+    return result
+  }
+
   render(result = []) {
-    result.push(this.print())
-    if (!this.isLeaf) {
-      this.split()
-      this.tl.render(result)
-      this.tr.render(result)
-      this.bl.render(result)
-      this.br.render(result)
+    this.build()
+    const queue = [this]
+
+    while (queue.length > 0) {
+      const node = queue.shift()
+
+      if (node) {
+        result.push(node.print())
+
+        if (!node.isLeaf) {
+          const children = node.getChildren()
+          queue.push(...children)
+        } else {
+          queue.push(null, null, null, null)
+        }
+      } else {
+        result.push(null)
+      }
+    }
+    // Trim trailing nulls
+    while (result[result.length - 1] === null) {
+      result.pop()
     }
     return result
   }
